9221fc64800838aa8743ef7b2c207f9626a0c9ef
David Hartman <david.j.hartman@gmail.com>
2022-09-22 08:09:22 -0400

    Checkin to save progress in Section 5.28.  Troubleshooting error related to product/id api call.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,32 @@
-import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
+import CartScreen from './screens/CartScreen';
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
-            <Route path="/product/:id" element={<ProductScreen />} />
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen/>} exact/>
+                        <Route path="/product/:id" element={<ProductScreen/>}/>
+                        <Route path="/cart/:id?" element={<CartScreen/>}/>
+                        <Route path="/cart" element={<CartScreen/>} >
+                            <Route path=":id" element={<CartScreen/>}></Route>
+                        </Route>
+                    </Routes>
+                </Container>
+            </main>
+            <Footer/>
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from 'react'
-import {Link, useParams} from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
-import {useDispatch, useSelector} from 'react-redux';
-import {listProductDetails} from '../actions/productActions'
+
+import { useDispatch, useSelector } from 'react-redux';
+import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-function ProductScreen() {
-    const match = useParams();
+function ProductScreen({ props}) {
+    const match = useParams()
+    const navigate = useNavigate()
+    const [qty, setQty] = useState(1)
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
 
@@ -19,6 +21,11 @@ function ProductScreen() {
     }, [dispatch, match])
 
     const { loading, error, product } = productDetails
+
+    const addToCartHandler = () => {
+        navigate(`/cart/${match.id}?qty=${qty}`)
+    }
+
     return (
         <div>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
@@ -69,11 +76,40 @@ function ProductScreen() {
                                         </Row>
                                     </ListGroup.Item>
 
+                                    {product.countInStock > 0 &&
+                                    (<ListGroup.Item>
+                                        <Row>
+                                            <Col>Qty:</Col>
+                                            <Col xs={'auto'} className={'my-1'}>
+                                                <Form.Control
+                                                    as={'select'}
+                                                    value={qty}
+                                                    onChange={(e) => setQty(e.target.value)}
+                                                >
+                                                    {
+                                                        [...Array(product.countInStock).keys()].map((x) => (
+                                                            <option key={x + 1} value={x + 1}>
+                                                                {x + 1}
+                                                            </option>
+                                                        ))
+                                                    }
+
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>)
+                                    }
+
                                     <ListGroup.Item>
                                         <Row>
-                                            <Button className={'btn-block'} disabled={product.countInStock == 0}
-                                                    type='button'>Add
-                                                to Cart</Button>
+                                            <Button
+                                                onClick={addToCartHandler}
+                                                className={'btn-block'}
+                                                disabled={product.countInStock == 0}
+                                                type='button'
+                                            >
+                                                Add to Cart
+                                            </Button>
                                         </Row>
                                     </ListGroup.Item>
                                 </ListGroup>
